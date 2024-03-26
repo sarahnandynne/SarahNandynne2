1bf7906396fb9108a7f9eccbff2592b7fca58151
SarahNandynne <miguelsarah051@gmail.com>
2024-03-26 09:34:46 -0300

    dia 26/03

---
+++ JogoSarah/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace JogoSarah;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
