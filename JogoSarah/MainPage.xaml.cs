ea69e8b6d9e42dc1d4301b48e090e2968f561935
SarahNandynne <miguelsarah051@gmail.com>
2024-04-09 10:41:08 -0300

    dia 08/04

--- JogoSarah/MainPage.xaml.cs
+++ JogoSarah/MainPage.xaml.cs
@@ -31,19 +31,24 @@ public partial class MainPage : ContentPage
 
 		personagens.Source = atual.GetArquivo();
 		pbAgua.Progress = atual.GetSede();
+		pbComida.Progress = atual.GetFome();
+		pbTristeza.Progress = atual.GetTristeza();
 		
 	}
 	void DarAgua(object sender, EventArgs args)
 	{
-
+		atual.SetSede(atual.GetSede() + 0.1);
+		pbAgua.Progress=atual.GetSede();
 	}
 	void DarComida(object sender, EventArgs args)
 	{
-
+		atual.SetFome(atual.GetFome() + 0.1);
+		pbComida.Progress=atual.GetFome();
 	}
 	void Brincar(object sender, EventArgs args)
 	{
-		
+		atual.SetTristeza(atual.GetTristeza() + 0.1);
+		pbTristeza.Progress=atual.GetTristeza();
 	}
 
 }
--- JogoSarah/Personagens.cs
+++ JogoSarah/Personagens.cs
@@ -6,6 +6,8 @@ public class Personagens
   protected double sede;
 
   protected string arquivo;
+  protected string arquivomorto;
+  protected bool morto;
     
   public Personagens()
   {
@@ -16,12 +18,62 @@ public class Personagens
 
   public string GetArquivo()
   {
-    return arquivo;
+    if (morto)
+      return arquivomorto;
+    else 
+      return arquivo;
   }
+    
 
   public double GetSede()
   {
     return sede;
   }
 
+public double GetTristeza()
+  {
+    return tristeza;
+  }
+  public double GetFome()
+  {
+    return fome;
+  }
+
+  public void SetSede( double s)
+  {
+    if (s>1)
+    sede = 1;
+    else if (s<0)
+    {
+      sede = 0;
+      morto = true;
+    }
+    else
+    sede = s;
+  }
+
+public void SetTristeza(double t)
+  {
+    if (t>1)
+    tristeza = 1;
+    else if (t<0)
+    {
+      tristeza = 0;
+      morto = true;
+    }
+    else
+    tristeza = t;
+  }
+  public void SetFome( double f)
+  {
+    if (f>1)
+    fome = 1;
+    else if (f<0)
+    {
+      fome = 0;
+      morto = true;
+    }
+    else
+    fome = f;
+  }
 }
